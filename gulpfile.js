8446a7a16324f8cdbb652b788eeb5843fdb9ae68
patrickhrastnik <30524365+patrickhrastnik@users.noreply.github.com>
2017-09-17 22:12:47 +0200

    Continued initial dev

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,11 @@ var pug = require('gulp-pug');
 var tsc = require('gulp-typescript');
 var sass = require('gulp-sass');
 
-gulp.task('patrickhrastnik.github.io:build:pug', () => {
+gulp.task('patrickhrastnik.github.io:pug:build', () => {
     return gulp.src('./src/pug/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('./'));
 });
+
+
+gulp.task('patrickhrastnik.github.io:build', ['patrickhrastnik.github.io:pug:build']);
